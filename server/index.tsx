9c1d3b3abaa7c2ff6ac4f25249653bcc4200a877
Matthew Underwood <matthewsunderwood@gmail.com>
2022-07-20 13:10:04 -0700

    add cors

--- server/index.tsx
+++ server/index.tsx
@@ -2,11 +2,14 @@ const express = require("express");
 const { exec } = require("child_process");
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const cors = require('cors')
+
 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ function App() {
   });
 
   function getConfig() {
-    fetch("/config")
+    fetch("http://localhost:3001/config")
       .then((res) => res.json())
       .then((data) => {
         setConfig(data.data)
@@ -184,7 +184,7 @@ function App() {
 
   function runCommand() {
     setLoading(true)
-    fetch("/command", {
+    fetch("http://localhost:3001/command", {
       method: "POST",
       body: JSON.stringify({ command: command}),
       headers: {
@@ -203,7 +203,7 @@ function App() {
   function updateInputs(e: SelectChangeEvent<unknown>) {
     const inputAmount = e.target.value as number;
     setLoading(true)
-    fetch("/inputs", {
+    fetch("http://localhost:3001/inputs", {
       method: "PUT",
       body: JSON.stringify({ inputAmount: inputAmount }),
       headers: {
@@ -218,7 +218,7 @@ function App() {
   function updateOutputs(e: SelectChangeEvent<unknown>) {
     const outputAmount = e.target.value as number;
     setLoading(true)
-    fetch("/outputs", {
+    fetch("http://localhost:3001/outputs", {
       method: "PUT",
       body: JSON.stringify({ outputAmount: outputAmount }),
       headers: {
